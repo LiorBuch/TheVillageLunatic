69c77c348833b816d35a0383ce3a3326bb31fddd
LiorBuch <lior.buchmeier@gmail.com>
2025-01-15 00:40:07 +0200

    feat: metadata tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,22 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>502 Bad Gateway</title>
+      <meta name="description" content="Your source for bizarre and hilarious fake news!" />
+
+      <meta property="og:url" content="https://thevillagelunatic.com/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="The Village Lunatic" />
+      <meta property="og:description" content="Your source for bizarre and hilarious fake news!" />
+      <meta property="og:image" content="/metadata_banner.png" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="thevillagelunatic.com" />
+      <meta property="twitter:url" content="https://thevillagelunatic.com/" />
+      <meta name="twitter:title" content="The Village Lunatic" />
+      <meta name="twitter:description" content="Your source for bizarre and hilarious fake news!" />
+      <meta name="twitter:image" content="/metadata_banner.png" />
+      <link rel="icon" href="site.png" />
       <body className="antialiased">
         <Main />
         <NextScript />
